71d0573f1dfa070e4d40bfc17d73bec6d4715402
DaanishZai <156246978+DaanishZai@users.noreply.github.com>
2024-01-25 13:24:21 -0800

    Update and rename StaticMethods.java to StaticMethod.java

--- StaticMethod.java
+++ StaticMethod.java
@@ -1,4 +1,4 @@
-class Quadrant{
+public class StaticMethod{
     public static int calc(double x, double y){
         if (x > 0 && y > 0){
             return 1;
@@ -13,5 +13,11 @@ class Quadrant{
             return 3;
         }
         return 0;
+    }
+    public static boolean isVowel(String s){
+      if (s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u") || s.equals("A") || s.equals("E") || s.equals("I") || s.equals("O") || s.equals("u")){
+        return true;
+      }
+      return false;
     }
 }
