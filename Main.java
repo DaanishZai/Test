d75dd5e31ea3f1f94436f667b243a427e2f280c1
DaanishZai <156246978+DaanishZai@users.noreply.github.com>
2024-01-25 13:09:40 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
   public static void main(String[] args){
-    System.out.print("Hello");
+    System.out.println("-4 and -17 are in quadrant " + Quadrant.calc(-4, -17));
+    System.out.println("4 and -17 are in quadrant " + Quadrant.calc(4, -17));
+    System.out.println("4 and 17 are in quadrant " + Quadrant.calc(4, 17));
+    System.out.println("-4 and 17 are in quadrant " + Quadrant.calc(-4, 17));
   }
 }
