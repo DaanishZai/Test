0740e21905417fdfdd869ca35bea22f39c8036ea
DaanishZai <156246978+DaanishZai@users.noreply.github.com>
2024-01-25 13:27:48 -0800

    Update StaticMethod.java

--- StaticMethod.java
+++ StaticMethod.java
@@ -15,9 +15,6 @@ public class StaticMethod{
         return 0;
     }
     public static boolean isVowel(String s){
-      if (s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u") || s.equals("A") || s.equals("E") || s.equals("I") || s.equals("O") || s.equals("u")){
-        return true;
-      }
-      return false;
+      return (s.equals("a") || s.equals("e") || s.equals("i") || s.equals("o") || s.equals("u") || s.equals("A") || s.equals("E") || s.equals("I") || s.equals("O") || s.equals("U"));
     }
 }
