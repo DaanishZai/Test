6f7164e542d19d9732919edfbb263206f0c1c0a6
DaanishZai <156246978+DaanishZai@users.noreply.github.com>
2024-01-25 13:24:38 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 public class Main{
   public static void main(String[] args){
-    System.out.println("-4 and -17 are in quadrant " + Quadrant.calc(-4, -17));
-    System.out.println("4 and -17 are in quadrant " + Quadrant.calc(4, -17));
-    System.out.println("4 and 17 are in quadrant " + Quadrant.calc(4, 17));
-    System.out.println("-4 and 17 are in quadrant " + Quadrant.calc(-4, 17));
-    System.out.println("0 and 0 are in quadrant " + Quadrant.calc(0, 0));
+    System.out.println("-4 and -17 are in quadrant " + StaticMethod.calc(-4, -17));
+    System.out.println("4 and -17 are in quadrant " + StaticMethod.calc(4, -17));
+    System.out.println("4 and 17 are in quadrant " + StaticMethod.calc(4, 17));
+    System.out.println("-4 and 17 are in quadrant " + StaticMethod.calc(-4, 17));
+    System.out.println("0 and 0 are in quadrant " + StaticMethod.calc(0, 0));
+
+    System.out.println("a is a vowel: " + StaticMethod.isVowel("a"));
+    System.out.println("e is a vowel: " + StaticMethod.isVowel("e"));
+    System.out.println("i is a vowel: " + StaticMethod.isVowel("i"));
+    System.out.println("o is a vowel: " + StaticMethod.isVowel("o"));
+    System.out.println("u is a vowel: " + StaticMethod.isVowel("u"));
+    System.out.println("A is a vowel: " + StaticMethod.isVowel("A"));
+    System.out.println("t is a vowel: " + StaticMethod.isVowel("t"));
   }
 }
-
